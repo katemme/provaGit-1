76283294f2210c446264cb59729359a1e5b0dd19
Damiano Azzolini <damiano.azzolini@unife.it>
2022-03-29 09:57:08 +0200

    abc

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Stampa");
         System.out.println("Stampa2");
+        System.out.println("Stampa3");
     }
 }
