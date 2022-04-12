ef3e8893264ed666524a9fb022c5a37ed8c47f3c
Damiano Azzolini <damiano.azzolini@unife.it>
2022-04-12 10:06:09 +0200

    progetto

---
+++ esempiStream/src/main/java/it/isa/stream/Stream.java
@@ -0,0 +1,12 @@
+package it.isa.stream;
+
+/**
+ * Hello world!
+ *
+ */
+public class Stream {
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
